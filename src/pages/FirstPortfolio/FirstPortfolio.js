4a6a9f62c8bb04a1d3aeae5c34ef49148eab976b
elias <elias.d.crocetti@gmail.com>
2023-06-14 12:50:58 -0300

    no me acuerdo que subi  pero tengo que hacer otra cosa. suerte bro del futuro xd

--- src/pages/FirstPortfolio/FirstPortfolio.js
+++ src/pages/FirstPortfolio/FirstPortfolio.js
@@ -1,21 +1,28 @@
 import {  useSearchParams } from 'react-router-dom';
 import { Imagen } from '../../components/Imagen/Imagen';
+import './FirstPortfolio.css'
 
 const FirstPortfolio = () => {  
   const [searchParams] = useSearchParams();
   console.log('searchParams', ); 
 
   return (
-    <div>  
-      <Imagen src={"https://pm1.aminoapps.com/6922/714394a04d4ef6ae60fb030685dcfed809931983r1-841-1024v2_uhq.jpg"} />
-   
-      <h2>Datos enviados: </h2>
-      <p>Nombre: {searchParams.get('name')}</p>
-      <p>GIT: {searchParams.get('git')}</p>
-      <p>linkedin: {searchParams.get('linkedin')}</p>
-      <p>proyects: {searchParams.get('proyects')}</p>
-      <p>skills: {searchParams.get('skills')}</p>
-      <p>description: {searchParams.get('description')}</p>
+    <div className='container'>  
+      <div className='leftDiv'>
+          <div className='imgClass'>  
+            <Imagen src={"https://pm1.aminoapps.com/6922/714394a04d4ef6ae60fb030685dcfed809931983r1-841-1024v2_uhq.jpg"} />
+          </div>
+          <div className='skillsClass'>              
+              <p>Nombre: {searchParams.get('name')}</p>
+              <p>GIT: {searchParams.get('git')}</p>
+              <p>linkedin: {searchParams.get('linkedin')}</p>
+              <p>skills: {searchParams.get('skills')}</p>
+          </div>
+      </div>
+      <div className='rightDiv'>
+            <div>proyects: {searchParams.get('proyects')}</div>
+            <div>description: {searchParams.get('description')}</div>
+      </div>
      </div>
   );
 };
