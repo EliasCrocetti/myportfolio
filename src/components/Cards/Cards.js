bfaef77c44e654c1021e18db9593709dbc0ff67b
elias <elias.d.crocetti@gmail.com>
2023-07-12 19:07:45 -0300

    hago que se pueda agregar cualquier tipo de imagen sin importar el formato.

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,14 +4,14 @@ import './Cards.css';
 const Card = (props) => {
   const supportedFormats = ['.jpg', '.jpeg', '.png', '.gif'];
   const imageDefault = 'https://c4.wallpaperflare.com/wallpaper/60/254/399/dragon-ball-z-son-goku-gohan-dragon-ball-wallpaper-preview.jpg';
-  const imageUrl = props.imageUrl && supportedFormats.some(format => props.imageUrl.endsWith(format)) ? props.imageUrl : imageDefault;
+  //const imageUrl = props.imageUrl && supportedFormats.some(format => props.imageUrl.endsWith(format)) ? props.imageUrl : imageDefault;
 
   const linkUrl = /^(http:\/\/|https:\/\/)/.test(props.link) ? props.link : `http://${props.link}`;
 
   return (
     <div className="card">
       {!props.contentTrabajo && (
-        <img src={imageUrl} alt="Imagen de la tarjeta" className="card-img" />
+        <img src={props.imageUrl} alt="Imagen de la tarjeta" className="card-img" />
       )}
       <div className="card-content">
         <h2 className="card-title">{props.title}</h2>
