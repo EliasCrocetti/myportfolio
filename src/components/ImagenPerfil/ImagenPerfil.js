848658488b84acb10a9e94b8531372cef022ba76
elias <elias.d.crocetti@gmail.com>
2023-07-12 19:01:41 -0300

    actualizo imagenperfil

--- src/components/ImagenPerfil/ImagenPerfil.js
+++ src/components/ImagenPerfil/ImagenPerfil.js
@@ -4,8 +4,6 @@ export const ImagenPerfil = (props) => {
   //recibo todas las propiedades de la img
   const { alt, ...otherProps } = props;
 
-  console.log(otherProps.src[0])
-
   return (
     <div>
     <img {...otherProps} alt={alt} className='imagenPerfil' />
